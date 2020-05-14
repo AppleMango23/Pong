fdcd21c1bcf49b4ea91c6754009e9be83fa29c8b
Noah Yek <ninjayek@gmail.com>
2020-05-14 12:07:20 +0800

    Pong 7

--- Paddle.lua
+++ Paddle.lua
@@ -1,9 +1,12 @@
 --[[
     GD50 2018
     Pong Remake
+
     -- Paddle Class --
+
     Author: Colton Ogden
     cogden@cs50.harvard.edu
+
     Represents a paddle that can move up and down. Used in the main
     program to deflect the ball back toward the opponent.
 ]]
@@ -14,8 +17,10 @@ Paddle = Class{}
     The `init` function on our class is called just once, when the object
     is first created. Used to set up all variables in the class and get it
     ready for use.
+
     Our Paddle should take an X and a Y, for positioning, as well as a width
     and height for its dimensions.
+
     Note that `self` is a reference to *this* object, whichever object is
     instantiated at the time this function is called. Different objects can
     have their own x, y, width, and height values, thus serving as containers
